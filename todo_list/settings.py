88e225b358cb50188fc303398ac609afa590e233
mihir-kig <mihirkantiroy939@gmail.com>
2025-05-23 00:52:02 +0600

    thirdcomit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #LOGIN_URL = 'login-page'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIRS = (os.path.join(BASE_DIR, 'static'), )
+
+django_heroku.settings(locals())
